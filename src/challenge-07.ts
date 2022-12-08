e3dc70ba4ab7a109bef82394319eb7e81a9e065f
iswilljr <iswilljr@gmail.com>
2022-12-07 19:44:24 -0500

    feat: add solution for challenge #07

--- src/challenge-07.ts
+++ src/challenge-07.ts
@@ -1,3 +1,5 @@
 export function getGiftsToRefill(a1: string[], a2: string[], a3: string[]) {
-  return [];
+  return [...new Set(a1.concat(a2, a3))].filter(
+    (gift) => (a1.includes(gift) as any) + a2.includes(gift) + a3.includes(gift) == 1
+  );
 }
