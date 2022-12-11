5cc461e8eb3674af23704cd39423b39a463b3246
iswilljr <iswilljr@gmail.com>
2022-12-11 16:31:55 -0500

    chore(test): add test for Challenge #08

---
+++ test/challenge-08.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest'
+import { checkPart } from '@/challenge-08'
+
+const TEST_CASES = [
+  { args: 'uwu', expected: true },
+  { args: 'will', expected: false },
+  { args: 'lolol', expected: true },
+  { args: 'yolooloy', expected: true },
+  { args: 'zzzoonzzz', expected: true },
+]
+
+describe('Challenge #8: We need a mechanic!', () => {
+  it('#T should return a boolean', () => {
+    expect(typeof checkPart('will')).toBe('boolean')
+  })
+
+  it.each(TEST_CASES)(
+    '# should return $expected when the input is $args',
+    ({ args, expected }) => {
+      expect(checkPart(args)).toEqual(expected)
+    }
+  )
+})
