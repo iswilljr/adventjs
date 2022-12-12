5afe0fc30eeb9ffaaaf6ec1920961da858e815bb
iswilljr <iswilljr@gmail.com>
2022-12-12 12:29:53 -0500

    feat: update solution for challenge #05

--- 2022/challenge-05.ts
+++ 2022/challenge-05.ts
@@ -1,41 +1,14 @@
-// score: 98 - solution 1
-export function getMaxGifts1(
-  giftsCities: number[],
-  maxGifts: number,
-  maxCities: number
-) {
-  return Math.max(
-    ...giftsCities
-      .sort((x, y) => y - x)
-      .reduce((result, _, i) => {
-        i && giftsCities.unshift(giftsCities.pop() as number)
-        i = giftsCities
-          .slice(0, maxCities)
-          .reduce((acc, curr) => (acc += curr), 0)
-
-        i <= maxGifts && result.push(i)
-        i - giftsCities[maxCities - 1] <= maxGifts &&
-          result.push(i - giftsCities[maxCities - 1])
-        return result
-      }, [] as number[]),
-    0
-  )
-}
-
-// score: 209 - partial solution 2
+// score: 217
 export function getMaxGifts(
   giftsCities: number[],
   maxGifts: number,
   maxCities: number
 ) {
-  return giftsCities.reduce(
-    (acc, value, _i, _a, v = acc[0] + value) => {
-      return v <= maxGifts && acc[1] < maxCities
-        ? [v, acc[1] + 1]
-        : acc[0] < value && value <= maxGifts
-        ? [value, 1]
-        : acc
-    },
-    [0, 0]
-  )[0]
+  return giftsCities
+    .sort((a, b) => b - a)
+    .reduce((a, v) => {
+      if (maxCities == 0 || a + v > maxGifts || a + v == maxGifts - 1) return a
+      maxCities--
+      return a + v
+    }, 0)
 }
