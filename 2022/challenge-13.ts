13c4945dc09efa50024e5a6b54c3dbefeca49b66
iswilljr <iswilljr@gmail.com>
2022-12-13 20:27:32 -0500

    feat: add solution for challenge #13

---
+++ 2022/challenge-13.ts
@@ -0,0 +1,10 @@
+export function getFilesToBackup(
+  lastBackup: number,
+  changes: [number, number][]
+) {
+  return [
+    ...new Set(
+      changes.filter(([, time]) => time > lastBackup).map(([id]) => id)
+    ),
+  ].sort((a, b) => a - b)
+}
