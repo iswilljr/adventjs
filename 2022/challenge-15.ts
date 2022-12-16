6c3065dac6c7a67e34ddc68698c7a08be8137156
iswilljr <iswilljr@gmail.com>
2022-12-16 10:25:06 -0500

    feat: add solution for challenge #15

---
+++ 2022/challenge-15.ts
@@ -0,0 +1,37 @@
+export function decorateTree(base: string) {
+  const dict = {
+    PP: 'P',
+    PR: 'B',
+    PB: 'R',
+    RR: 'R',
+    RP: 'B',
+    RB: 'P',
+    BB: 'B',
+    BP: 'R',
+    BR: 'P',
+  }
+
+  return base
+    .split(' ')
+    .slice(1)
+    .reduce(
+      tree => {
+        const topTree = tree[0].split(' ')
+
+        return [
+          topTree
+            .slice(0, -1)
+            .reduce(
+              (acc, letter, i) =>
+                acc.concat(
+                  dict[(letter + topTree[i + 1]) as keyof typeof dict]
+                ),
+              [] as string[]
+            )
+            .join(' '),
+          ...tree,
+        ]
+      },
+      [base]
+    )
+}
