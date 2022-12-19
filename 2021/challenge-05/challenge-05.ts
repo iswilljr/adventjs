3976e364f8b47c788b6b2d8ccb0721d9dfb99d8f
Will <iswilljr@gmail.com>
2022-12-19 16:11:36 -0600

    fix(2021): challenge #05 typo

--- 2021/challenge-05/challenge-05.ts
+++ 2021/challenge-05/challenge-05.ts
@@ -2,7 +2,7 @@ export function daysToXmas(date: Date) {
   const getDays = (time: number) => Math.floor(time / 1000 / 60 / 60 / 24)
 
   const xmasDays = getDays(new Date('Dec 25, 2021').getTime())
-  const dateDaya = getDays((date.setHours(0), date.getTime()))
+  const dateDays = getDays((date.setHours(0), date.getTime()))
 
-  return xmasDays - dateDaya
+  return xmasDays - dateDays
 }
