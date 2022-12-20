e6f77392150801e55f132c4578ac7784e5cbcdc6
iswilljr <iswilljr@gmail.com>
2022-12-20 15:11:06 -0500

    feat: update solution for challenge #14

--- 2022/challenge-14/challenge-14.ts
+++ 2022/challenge-14/challenge-14.ts
@@ -1,8 +1,8 @@
 // score: 400
 export function getOptimalPath(path: number[][]) {
-  return path.reduceRight((previous, current) =>
-    current.map(
-      (value, index) => value + Math.min(previous[index], previous[index + 1])
-    )
-  )[0]
+  return path.reduceRight((previous, current) => {
+    return current.map((value, index) => {
+      return value + Math.min(previous[index], previous[index + 1])
+    })
+  })[0]
 }
