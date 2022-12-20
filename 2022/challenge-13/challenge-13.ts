dadf8e18e20ae3ea30e3156f71a286918490cfb5
iswilljr <iswilljr@gmail.com>
2022-12-20 15:10:19 -0500

    feat: update solution for challenge #13

--- 2022/challenge-13/challenge-13.ts
+++ 2022/challenge-13/challenge-13.ts
@@ -3,9 +3,8 @@ export function getFilesToBackup(
   lastBackup: number,
   changes: Array<[number, number]>
 ) {
-  return [
-    ...new Set(
-      changes.filter(([, time]) => time > lastBackup).map(([id]) => id)
-    ),
-  ].sort((a, b) => a - b)
+  const ids = changes.filter(([, time]) => time > lastBackup).map(([id]) => id)
+  const uniqueIds = [...new Set(ids)]
+
+  return uniqueIds.sort((a, b) => a - b)
 }
