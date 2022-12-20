bee6bdf35873dcfac76069cdec76576bc6ebb603
iswilljr <iswilljr@gmail.com>
2022-12-20 14:56:53 -0500

    feat: update solution for challenge #03

--- 2022/challenge-03/challenge-03.ts
+++ 2022/challenge-03/challenge-03.ts
@@ -1,6 +1,7 @@
 // score: 175
 export function distributeGifts(packOfGifts: string[], reindeers: string[]) {
-  return Math.floor(
-    (reindeers.join('').length * 2) / packOfGifts.join('').length
-  )
+  const reindeersWight = reindeers.join('').length * 2
+  const packWeight = packOfGifts.join('').length
+
+  return Math.floor(reindeersWight / packWeight)
 }
