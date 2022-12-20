230e10ba7660fb55c2ed7a71691577e1688f00bb
iswilljr <iswilljr@gmail.com>
2022-12-20 15:22:45 -0500

    feat: update solution for challenge #17

--- 2022/challenge-17/challenge-17.ts
+++ 2022/challenge-17/challenge-17.ts
@@ -1,14 +1,14 @@
 export function carryGifts(gifts: string[], maxWeight: number) {
   return gifts
     .filter(g => g.length <= maxWeight)
-    .reduce<string[]>(
-      (b, g) => (
-        b.length > 0 &&
-        (b.at(-1) as string).replace(/\s/g, '').length + g.length <= maxWeight
-          ? (b[b.length - 1] += ` ${g}`)
-          : b.push(g),
-        b
-      ),
-      []
-    )
+    .reduce<string[]>((bags, bag) => {
+      const lastBag = bags.at(-1)
+      const lastBagLength = (lastBag && lastBag.replace(/\s/g, '').length) || 0
+
+      if (bags.length > 0 && lastBagLength + bag.length <= maxWeight) {
+        bags[bags.length - 1] += ` ${bag}`
+      } else bags.push(bag)
+
+      return bags
+    }, [])
 }
