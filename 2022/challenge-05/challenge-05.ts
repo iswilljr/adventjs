ce83fd70492ee826108ceb282013052b16693b31
iswilljr <iswilljr@gmail.com>
2022-12-20 15:00:53 -0500

    feat: update solution for challenge #05

--- 2022/challenge-05/challenge-05.ts
+++ 2022/challenge-05/challenge-05.ts
@@ -6,12 +6,13 @@ export function getMaxGifts(
 ) {
   return giftsCities
     .sort((a, b) => b - a)
-    .reduce((a, v) => {
-      if (maxCities === 0 || a + v > maxGifts || a + v === maxGifts - 1) {
-        return a
+    .reduce((acc, gift) => {
+      const total = acc + gift
+      if (maxCities === 0 || total > maxGifts || total === maxGifts - 1) {
+        return acc
       }
 
       maxCities--
-      return a + v
+      return total
     }, 0)
 }
