fa04efe86fcade1a7e46e50fca108e1df878ae78
iswilljr <iswilljr@gmail.com>
2022-12-20 14:53:00 -0500

    feat: update solution for challenge #06

--- 2022/challenge-06/challenge-06.ts
+++ 2022/challenge-06/challenge-06.ts
@@ -1,14 +1,16 @@
 // score: 200
 export function createCube(size: number) {
-  return [
-    ...Array.from(
-      { length: size },
-      (_v, i) =>
-        `${' '.repeat(size - i - 1)}${'/\\'.repeat(i + 1)}${'_\\'.repeat(size)}`
-    ),
-    ...Array.from(
-      { length: size },
-      (_v, i) => `${' '.repeat(i)}${'\\/'.repeat(size - i)}${'_/'.repeat(size)}`
-    ),
-  ].join('\n')
+  const cube: string[] = []
+  const topRight = '_\\'.repeat(size)
+  const bottomRight = '_/'.repeat(size)
+
+  for (let i = 0; i < size; i++) {
+    const space = ' '.repeat(i)
+    const borderSize = size - i
+
+    cube.unshift(`${space}${'/\\'.repeat(borderSize)}${topRight}`)
+    cube.push(`${space}${'\\/'.repeat(borderSize)}${bottomRight}`)
+  }
+
+  return cube.join('\n')
 }
