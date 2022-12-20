df59b013f0ab548126804eedbcf0ffbae036ef6d
iswilljr <iswilljr@gmail.com>
2022-12-20 14:46:58 -0500

    feat: update solution for challenge #02

--- 2022/challenge-02/challenge-02.ts
+++ 2022/challenge-02/challenge-02.ts
@@ -1,20 +1,4 @@
 // score: 121
 export function countHours(year: number, holidays: string[]) {
-  return holidays
-    .slice(0, Math.ceil(holidays.length / 2))
-    .reduce(
-      (hours, holiday, i) => (
-        !/0|6/.test(`${new Date(`${holiday}/${year}`).getDay()}`) &&
-          (hours += 2),
-        holidays.length - i - 1 !== i &&
-          !/0|6/.test(
-            `${new Date(
-              `${holidays[holidays.length - i - 1]}/${year}`
-            ).getDay()}`
-          ) &&
-          (hours += 2),
-        hours
-      ),
-      0
-    )
+  return holidays.filter(d => new Date(`${year}/${d}`).getDay() % 6).length * 2
 }
