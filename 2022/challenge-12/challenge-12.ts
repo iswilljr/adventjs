55c62d6de65e4b9374bdc6162ec3941c0fa1c93e
iswilljr <iswilljr@gmail.com>
2022-12-20 15:08:23 -0500

    feat: update solution for challenge #12

--- 2022/challenge-12/challenge-12.ts
+++ 2022/challenge-12/challenge-12.ts
@@ -1,13 +1,14 @@
 // score: 320
 export function selectSleigh(distance: number, sleighs: Sleight[]) {
   let maxW = 20
-  return sleighs.reduce<null | string>(
-    (acc, s) =>
-      s.consumption * distance > maxW
-        ? ((maxW = s.consumption * distance), acc)
-        : s.name,
-    null
-  )
+  return sleighs.reduce<null | string>((acc, s) => {
+    if (s.consumption * distance > maxW) {
+      maxW = s.consumption * distance
+      return acc
+    }
+
+    return s.name
+  }, null)
 }
 
 export interface Sleight {
