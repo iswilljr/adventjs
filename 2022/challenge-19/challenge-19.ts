00e79e96bc142f804e3c5ab88d9d1dc581025ba0
iswilljr <iswilljr@gmail.com>
2022-12-20 15:23:13 -0500

    feat: update solution for challenge #19

--- 2022/challenge-19/challenge-19.ts
+++ 2022/challenge-19/challenge-19.ts
@@ -1,7 +1,8 @@
 export function sortToys(toys: string[], positions: number[]) {
   const min = Math.min(...positions)
-  return toys.reduce<string[]>(
-    (acc, toy, i) => ((acc[positions[i] - min] = toy), acc),
-    []
-  )
+
+  return toys.reduce<string[]>((acc, toy, i) => {
+    acc[positions[i] - min] = toy
+    return acc
+  }, [])
 }
