e79fb7904042ea9bd466fde35a4d3747680c95c3
iswilljr <iswilljr@gmail.com>
2022-12-21 12:33:20 -0500

    feat(2021): add update for challenge #21

--- 2021/challenge-21/challenge-21.ts
+++ 2021/challenge-21/challenge-21.ts
@@ -2,14 +2,12 @@ export function canCarry(capacity: number, trip: number[][]) {
   const path: number[] = []
   const sum = (arr: number[]) => arr.reduce((acc, n) => acc + n, 0)
 
-  return trip.reduce((canCarry, [gifts, point, place]) => {
+  return trip.every(([gifts, point, place]) => {
     path[point] = gifts + (path[point] || 0)
     path[place] = -gifts
 
     const prevPointGifts = path.slice(0, point + 1)
 
-    if (gifts > capacity || sum(prevPointGifts) > capacity) canCarry = false
-
-    return canCarry
-  }, true)
+    return gifts <= capacity && sum(prevPointGifts) <= capacity
+  })
 }
