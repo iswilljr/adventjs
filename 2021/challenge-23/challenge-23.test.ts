342abe2e21ab0899adf8e8208ba95195b50d395b
iswilljr <iswilljr@gmail.com>
2023-03-06 19:51:57 -0500

    test(2021): add new test case for challenge #23

--- 2021/challenge-23/challenge-23.test.ts
+++ 2021/challenge-23/challenge-23.test.ts
@@ -5,6 +5,7 @@ const TEST_CASES: TestCases<[string, string], boolean> = [
   { args: ['BAL', 'LIB'], expected: true },
   { args: ['CON', 'JUU'], expected: false },
   { args: ['MMM', 'MID'], expected: false },
+  { args: ['BR', 'BAD'], expected: false },
 ]
 
 describe('Challenge #23: ¿Puedes reconfigurar las fábricas para no parar de crear regalos?', () => {
