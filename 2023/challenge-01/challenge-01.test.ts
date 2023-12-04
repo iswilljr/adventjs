f739b7ceea1075f9ef8fb637f18ddff7aafc1cde
iswilljr <iswilljr@gmail.com>
2023-12-04 12:09:19 -0500

    feat(2023): update challenge #01 and #03 tests

--- 2023/challenge-01/challenge-01.test.ts
+++ 2023/challenge-01/challenge-01.test.ts
@@ -15,6 +15,8 @@ const TEST_CASES: TestCases<number[], number> = [
   { args: [12, 20, 30, 11, 20, 12], expected: 20 },
   { args: [12, 10, 13, 12, 13], expected: 12 },
   { args: [0, 10, 13, 0, 13], expected: 0 },
+  { args: [1, 3, 4, 5, 0, 1, 3, 0, 7], expected: 1 },
+  { args: [1, 10, 2, 10, 20, 50, 7, 0, 0, 7], expected: 10 },
 ]
 
 describe('Challenge #1: First gift repeated!', () => {
--- 2023/challenge-03/challenge-03.test.ts
+++ 2023/challenge-03/challenge-03.test.ts
@@ -3,9 +3,11 @@ import { findNaughtyStep } from './challenge-03'
 
 const TEST_CASES: TestCases<[string, string], string> = [
   { args: ['abcd', 'abcde'], expected: 'e' },
+  { args: ['abcde', 'abcd'], expected: 'e' },
   { args: ['xxxx', 'xxoxx'], expected: 'o' },
   { args: ['stepfor', 'stepor'], expected: 'f' },
   { args: ['iiiii', 'iiiii'], expected: '' },
+  { args: ['iiii', 'iiiii'], expected: 'i' },
 ]
 
 describe('Challenge #3: The naughty elf', () => {
