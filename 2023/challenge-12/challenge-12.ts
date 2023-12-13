3fce6d6bbaf6ac5d36d677e7188c71b2d953466f
iswilljr <iswilljr@gmail.com>
2023-12-13 09:48:18 -0500

    feat(2023): update challenge #12

--- 2023/challenge-12/challenge-12.ts
+++ 2023/challenge-12/challenge-12.ts
@@ -1,18 +1,20 @@
 export function checkIsValidCopy(original: string, copy: string) {
   let index = 0
   let isValidCopy = true
+  const symbolSequence = '#+:. '
 
   for (const letter of original) {
     const copyLetter = copy[index++]
+    const symbolIndex = symbolSequence.indexOf(letter)
 
-    const isValidLetter = [
-      letter.toLowerCase(),
-      '#',
-      '+',
-      ':',
-      '.',
-      ' ',
-    ].includes(copyLetter)
+    const symbols = [
+      symbolSequence,
+      symbolSequence.slice(symbolIndex, symbolSequence.length),
+    ][+(symbolIndex !== -1)]
+
+    const isValidLetter = `${letter}${letter.toLowerCase()}${symbols}`.includes(
+      copyLetter,
+    )
 
     const isLetterBlankSpace = letter === ' '
     const isCopyLetterBlankSpace = copyLetter === ' '
