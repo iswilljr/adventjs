fc1694b682f7c508221e730453b01cd4be3eb004
iswilljr <iswilljr@gmail.com>
2023-12-25 11:17:22 -0500

    feat(2023): update solution for challenge #25

--- 2023/challenge-25/challenge-25.ts
+++ 2023/challenge-25/challenge-25.ts
@@ -3,25 +3,22 @@
 export function travelDistance(map: string) {
   const roadmapArr = map.split('\n')
   const roadmap = roadmapArr.join('')
-  const rows = roadmapArr.length
   const cols = roadmapArr[0].length
-  const len = roadmap.length
   const santaPos = roadmap.indexOf('S')
 
   let gift = 1
   let movements = 0
   let santaCol = santaPos % cols
-  let santaRow = Math.floor((santaPos * rows) / len)
+  let santaRow = Math.floor(santaPos / cols)
 
   const numbers = roadmap.replace(/\.|S/g, '')
 
   for (const _number of numbers) {
     const giftPos = roadmap.indexOf(`${gift}`)
     const giftCol = giftPos % cols
-    const giftRow = Math.floor((giftPos * rows) / len)
+    const giftRow = Math.floor(giftPos / cols)
 
-    movements += Math.abs(santaRow - giftRow)
-    movements += Math.abs(santaCol - giftCol)
+    movements += Math.abs(santaRow - giftRow) + Math.abs(santaCol - giftCol)
 
     santaCol = giftCol
     santaRow = giftRow
